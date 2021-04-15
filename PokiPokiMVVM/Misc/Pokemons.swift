c35ae4ed85ca055f34a701787aa3172671932023
RJ <r.j.chomik@gmail.com>
2021-04-15 10:20:32 +0100

    model implementation

---
+++ PokiPokiMVVM/Misc/Pokemons.swift
@@ -0,0 +1,41 @@
+//
+//  Pokemons.swift
+//  PokiPokiMVVM
+//
+//  Created by The App Experts on 15/04/2021.
+//
+
+import Foundation
+
+struct Pokiemonies: Codable {
+    let results: [Result]
+}
+
+// MARK: - Result
+struct Result: Codable {
+    let name: String
+    let url: String
+}
+
+// MARK: - Model
+struct Pokemon: Codable {
+    let id: Int
+    let name: String
+    let baseExperience: Int
+    let height: Int
+    let weight: Int
+    let sprites: Sprites
+    
+    enum CodingKeys: String, CodingKey {
+            case id, name, height, weight, sprites
+            case baseExperience = "base_experience"
+        }
+}
+
+class Sprites: Codable {
+    let frontDefault: String
+
+    enum CodingKeys: String, CodingKey {
+        case frontDefault = "front_default"
+    }
+}
