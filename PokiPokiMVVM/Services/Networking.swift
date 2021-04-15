9ae156df6e0fc881e29608e7dadd10cb1074572d
RJ <r.j.chomik@gmail.com>
2021-04-15 11:00:50 +0100

    networking class implementation

--- PokiPokiMVVM/Services/Networking.swift
+++ PokiPokiMVVM/Services/Networking.swift
@@ -10,7 +10,9 @@ import Foundation
 
 enum NetErrors: Error {
     case badURL(message:String)
-    
+    case responsError(code:String)
+    case noData(message:String)
+    case decodingError(message:String)
 }
 protocol NetworkingProtocol {
     func getURL (_ host: String, _ path:String) -> URL?
@@ -19,11 +21,34 @@ protocol NetworkingProtocol {
 
 class Networking: NetworkingProtocol {
     func getURL(_ host: String, _ path: String) -> URL? {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = host
+        components.path = path
         
+        return components.url
     }
     
     func getModels(_ url: URL?) -> Future<Pokiemonies, NetErrors> {
         
+        return Future<Pokiemonies, NetErrors> {promise in
+            guard let url = url else {return promise(.failure(.badURL(message: "URL could not be unwraped"))) }
+            URLSession.shared.dataTask(with: url) { (data, respons, error) in
+                if let respons = respons as?  HTTPURLResponse, respons.statusCode != 200 {
+                    return promise(.failure(.responsError(code: "Session return code > \(respons.statusCode)")))
+                }
+                
+                guard let data = data else { return promise(.failure(.noData(message: "No data downloded")))}
+                
+                do {
+                    guard let result = try? JSONDecoder().decode(Pokiemonies.self, from: data) else {return promise(.failure(.decodingError(message: "Could not decode data")))}
+                    DispatchQueue.main.async {
+                        return promise(.success(result))
+                    }
+                }
+                
+            }.resume()
+        }
     }
     
     
